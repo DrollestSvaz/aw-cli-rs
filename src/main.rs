de72190fe228b0050068d00ce5d66826cd8504f0
Antony <antoninon524@gmail.com>
2026-03-05 16:35:41 +0100

    Introdotto sistema config.

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,26 @@ mod search_results;
 mod episode_list;
 mod post_play;
 
-
-
+#[cfg(target_os = "windows")]
+use std::path::Path;
+#[cfg(target_os = "windows")]
+use std::process::{Command, Stdio};
 use crossterm::execute;
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::cursor::MoveTo;
+// use crossterm::style::Stylize;
+#[cfg(target_os = "windows")]
+use serde::{Deserialize, Serialize };
+#[cfg(target_os = "windows")]
+use colored::Colorize;
+#[cfg(target_os = "windows")]
+use dirs::config_dir;
+#[cfg(target_os = "windows")]
+#[derive(Serialize, Deserialize, Debug)]
+struct Config {
+    vlc_path: Option<String>,
+    fzf_installed: bool,
+}
 enum State {
     SearchInput,
     SearchResults(Option<String>),
@@ -19,6 +34,8 @@ enum State {
 
 #[tokio::main]
 async fn main() {
+    #[cfg(target_os = "windows")]
+    config_creator();
 
 
     let args: Vec<String> = std::env::args().collect();
@@ -56,10 +73,91 @@ async fn main() {
         }
     }
 }
-fn vlc_path() -> &'static str {
-    if cfg!(target_os = "windows") {
-        r"C:\Program Files\VideoLAN\VLC\vlc.exe"
-    } else {
-        "vlc"
+
+#[cfg(target_os = "windows")]
+fn config_creator()  {
+
+    //Riutilizzo la variabile path per non sprecare memoria.
+    if !dirs::config_dir().unwrap().join("aw-cli-rs").exists() {
+        let mut vlc_path: String = String::new();
+        let mut path: String = String::new();
+        println!("{}", "Non è stato trovato il file di configurazione, vuoi procedere creandolo? (S/n)".red().bold());
+        std::io::stdin().read_line(&mut path).unwrap();
+        if path.to_lowercase().trim().eq("n") {
+            std::process::exit(0);
+        }
+
+        println!("{}", "Hai già vlc installato? (s/N)".bold().on_white().truecolor(37, 37, 37));
+        path.clear();
+        std::io::stdin().read_line(&mut path).unwrap();
+        if path.to_lowercase().trim().eq("s") {
+            let mut control = false;
+            println!("{}", "Inserire percorso assoluto di VLC:");
+            path.clear();
+            std::io::stdin().read_line(&mut path).unwrap();
+            while !control {
+
+                if Path::new(&path.trim()).exists() { control = true; vlc_path = path.clone(); }
+                else {
+                    println!("{}", "Path inserito non esistente!".red().bold());
+                    println!("{}", "Reinserire...");
+                    path.clear();
+                    std::io::stdin().read_line(&mut path).unwrap();
+
+                }
+            }
+        } else {
+            println!("{}", "Permetteresti l'app di installarlo al posto tuo? (S/n)".on_white().bold().truecolor(37, 37, 37));
+            path.clear();
+            std::io::stdin().read_line(&mut path).unwrap();
+            if path.to_lowercase().trim().eq("n") {
+                println!("A presto. In attesa che scarichi VLC.");
+                std::process::exit(0);
+            } else {
+                Command::new("winget").arg("install").arg("VideoLAN.VLC").spawn().unwrap().wait().unwrap();
+                println!("{}", "Installazione terminata.");
+                vlc_path = String::from(r"C:\Program Files\VideoLAN\VLC\vlc.exe");
+            }
+
+        }
+
+        let fzf_installed = Command::new("where")
+            .arg("fzf")
+            .stdout(Stdio::null())
+            .stderr(Stdio::null())
+            .status()
+            .map(|s| s.success())
+            .unwrap_or(false);
+        if !fzf_installed {
+            println!("{}", "Installazione di fzf, attendere...".bold());
+            Command::new("winget").arg("install").arg("fzf").spawn().unwrap().wait().unwrap();
+            println!("{}", "Installazione terminata.");
+        }
+
+        let config = Config {
+            vlc_path: Option::from(vlc_path.trim().to_string()),
+            fzf_installed: true,
+        };
+
+        let config_dir = dirs::config_dir().unwrap().join("aw-cli-rs");
+        std::fs::create_dir_all(&config_dir).unwrap();
+        let config_file = config_dir.join("config.toml");
+
+        let toml = toml::to_string(&config).unwrap();
+        std::fs::write(config_file, toml).unwrap();
+
     }
+}
+
+#[cfg(target_os = "windows")]
+pub fn vlc_path() -> String {
+    let content = std::fs::read_to_string(dirs::config_dir().unwrap().join(r"aw-cli-rs\config.toml")).unwrap();
+    let config: Config = toml::from_str(&content).unwrap();
+    config.vlc_path.unwrap()
+}
+
+
+#[cfg(not(target_os = "windows"))]
+pub fn vlc_path() -> String {
+    "vlc".to_string()
 }
